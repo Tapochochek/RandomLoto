b1f1d99f59d0f637a129b30a52cabbec16bad2a3
Nikonov Roman <156935220+Tapochochek@users.noreply.github.com>
2025-10-23 09:20:49 +0500

    Update API endpoint port in LottoMachine component

--- randomtrust_frontend_final_fixed/src/components/LottoMachine.jsx
+++ randomtrust_frontend_final_fixed/src/components/LottoMachine.jsx
@@ -20,7 +20,7 @@ export default function LottoMachine({mouseEvents}) {
     setRunning(true)
     setNumbers([])
     try{
-      const res = await fetch(`http://80.93.62.78:5014/api/random/lottery/${count}`)
+      const res = await fetch(`http://80.93.62.78:8080/api/random/lottery/${count}`)
 
       if(!res.ok) throw new Error('api')
       const arr = await res.json()
